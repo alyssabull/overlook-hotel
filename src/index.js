cd6e6ce8f24ab7887d95438d3815ff44d677fb46
Alyssa Bull <alyssabull@gmail.com>
2020-11-04 19:15:48 -0700

    Add user login functionality

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import './css/base.scss';
 
 let enterCredentials = document.querySelector('.enter-credentials');
 let managerView = document.querySelector('.manager-view');
+let customerView = document.querySelector('.customer-view');
 let usernameInput = document.querySelector('.username');
 let passwordInput = document.querySelector('.password');
 let submitButton = document.querySelector('.submit-button')
@@ -12,6 +13,9 @@ function validateCredentials() {
   if (usernameInput.value === 'manager' && passwordInput.value === 'overlook2020') {
     enterCredentials.classList.add('hidden');
     managerView.classList.remove('hidden');
+  } else if (usernameInput.value.includes('customer') && passwordInput.value === 'overlook2020') {
+    enterCredentials.classList.add('hidden');
+    customerView.classList.remove('hidden');
   } else {
     usernameInput.value = '';
     passwordInput.value = '';
